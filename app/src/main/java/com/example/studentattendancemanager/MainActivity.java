502a84698b364e17451e8bc362d1539febe9d2ae
Straysio <153062913+Straysio@users.noreply.github.com>
2023-12-06 18:42:24 +0300

    Add files via upload

--- app/src/main/java/com/example/studentattendancemanager/MainActivity.java
+++ app/src/main/java/com/example/studentattendancemanager/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.studentattendancemanager;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.widget.SimpleCursorAdapter;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -14,6 +17,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText login;
     private EditText password;
     private Button entrance_button;
+    DatabaseHelper databaseHelper;
+    SQLiteDatabase db;
+    Cursor userCursor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,9 +30,23 @@ public class MainActivity extends AppCompatActivity {
         password = findViewById(R.id.text_password);
         entrance_button = findViewById(R.id.entrance_button);
 
+        databaseHelper = new DatabaseHelper(getApplicationContext());
+        // создаем базу данных
+        databaseHelper.create_db();
+        db = databaseHelper.open();
+
         entrance_button.setOnClickListener(v -> {
             String login_str = login.getText().toString();
-            if (login_str.equals("teacher")) {
+            userCursor = db.rawQuery("select * from " + DatabaseHelper.TABLE, null);
+            boolean flag = false;
+            if (userCursor.moveToFirst()) {
+                do {
+                    if (login_str.equals(userCursor.getString(userCursor.getColumnIndexOrThrow(DatabaseHelper.COLUMN_LOGIN)))){
+                        flag = true;
+                    }
+                } while (userCursor.moveToNext());
+            }
+            if (flag) {
                 Intent intent = new Intent(MainActivity.this, HomePageTeacher.class);
                 startActivity(intent);
             } else {
@@ -34,6 +54,12 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Закрываем подключение и курсор
+        db.close();
+        userCursor.close();
     }
 }
